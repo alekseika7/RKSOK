81b6f9f4fbe8a803adf64bc5e081f304c6c62a74
Алексей Козлов <a.kozlov@veeroute.com>
2022-08-04 23:39:09 +0300

    logger

---
+++ service/logger.py
@@ -0,0 +1,15 @@
+from loguru import logger
+
+from config import SOURCE_DIR_PATH
+
+_format = '{time:YYYY:MM:DD:HH:mm:ss:SSS} | <level>{level: <8}</level> | {module}:{function}:{line} - ' \
+          '<level>{message}</level>'
+logger.remove(0)
+logger.add(
+    SOURCE_DIR_PATH + '/logs/server.log',
+    format=_format,
+    level='DEBUG',
+    enqueue=True,
+    rotation='1 MB',
+    compression='zip',
+)
