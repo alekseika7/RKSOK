e1205ce06348c9063a0b42b22346ceed9b135793
Алексей Козлов <a.kozlov@veeroute.com>
2022-08-04 23:36:58 +0300

    env variables

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+from os import path, getenv
+
+SOURCE_DIR_PATH = path.dirname(path.abspath(__file__))
+
 # Server conf
-SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 8888
+SERVER_HOST = getenv('RKSOK_SERVER_HOST')
+SERVER_PORT = int(getenv('RKSOK_SERVER_PORT'))
 CLIENT_REQUEST_TIMEOUT = 60
 COMMAND_EXEC_TIMEOUT = 10
 REQUEST_END = '\r\n\r\n'
@@ -11,7 +15,6 @@ CONTROL_SERVER_HOST = 'vragi-vezde.to.digital'
 CONTROL_SERVER_PORT = 51624
 
 # DB conf
-MONGO_CONNECTION_URI = 'mongodb://localhost:27017'
+MONGO_CONNECTION_URI = getenv('MONGO_CONNECTION_URI')
 MONGO_CONNECTION_MS_TIMEOUT = 15000
 MONGO_DB_NAME = 'phone_numbers'
-
