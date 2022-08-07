d64936b69770033a2f8a7dcdbd4ef5201aa7beb5
Алексей Козлов <a.kozlov@veeroute.com>
2022-08-07 21:07:44 +0300

    changed callback

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import asyncio
+from asyncio import StreamReader, StreamWriter
 
 from config import SERVER_HOST, SERVER_PORT, ENCODING
 from exceptions import IncorrectHostError, IncorrectPortError, ServerBaseException
@@ -8,10 +9,12 @@ from service.logger import logger
 from service.request_handler import process_client_request
 from utils import check_host_port
 
-_mongo_client = RKSOKMongoClient()
 
-
-async def process_request(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+async def process_request(
+        reader: StreamReader,
+        writer: StreamWriter,
+        db_client: RKSOKMongoClient,
+) -> None:
     """Callback for asyncio streams server."""
     try:
         client_address = writer.get_extra_info('peername')
@@ -19,9 +22,9 @@ async def process_request(reader: asyncio.StreamReader, writer: asyncio.StreamWr
         request = await read_data_with_timeout(reader=reader)
         logger.info(f'Received {request!r} from {client_address!r}')
 
-        await _mongo_client.connect_to_db(user_id=client_address[0])
+        await db_client.connect_to_db(user_id=client_address[0])
         logger.debug('Starting request processing...')
-        response = await process_client_request(request=request, db_client=_mongo_client)
+        response = await process_client_request(request=request, db_client=db_client)
         logger.info(f'Send {response!r} to {client_address!r}')
     except ServerBaseException as server_exception:
         logger.error(f'Exception happened: {server_exception}')
@@ -39,7 +42,12 @@ async def main() -> None:
     except (IncorrectHostError, IncorrectPortError) as connection_data_error:
         logger.error(f'Exception during checking host and port of the server: {connection_data_error}')
         raise KeyboardInterrupt
-    server = await asyncio.start_server(client_connected_cb=process_request, host=SERVER_HOST, port=int(SERVER_PORT))
+    db_client = RKSOKMongoClient()
+    server = await asyncio.start_server(
+        lambda reader, writer: process_request(reader=reader, writer=writer, db_client=db_client),
+        host=SERVER_HOST,
+        port=int(SERVER_PORT),
+    )
     logger.debug(f'Started RKSOK server on {SERVER_HOST}:{SERVER_PORT}')
     async with server:
         await server.serve_forever()
