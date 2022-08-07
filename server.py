40e0261d7cbe59c898c61f29b681a405e23fbb23
Алексей Козлов <a.kozlov@veeroute.com>
2022-08-07 19:03:03 +0300

    deleted RKSOK_type and control_server_conf, added host and port verification

--- server.py
+++ server.py
@@ -1,27 +1,17 @@
 import asyncio
-from asyncio import StreamReader, StreamWriter
 
-from config import SERVER_HOST, SERVER_PORT, CONTROL_SERVER_PORT, CONTROL_SERVER_HOST
-from exceptions import ServerBaseException
-from models.models import ControlServerConf, ControlServerResponse
+from config import SERVER_HOST, SERVER_PORT, ENCODING
+from exceptions import IncorrectHostError, IncorrectPortError, ServerBaseException
+from service.data_reader import read_data_with_timeout
 from service.db import RKSOKMongoClient
 from service.logger import logger
-from service.protocols import RKSOKProtocolFirstVersion
-from service.request_handler import process_client_request, read_data_with_timeout
-
-mongo_client = RKSOKMongoClient()
-control_server_conf = ControlServerConf(
-    host=CONTROL_SERVER_HOST,
-    port=CONTROL_SERVER_PORT,
-    ask_command='АМОЖНА?',
-    protocol='РКСОК/1.0',
-    responses=ControlServerResponse(yes='МОЖНА', no='НИЛЬЗЯ')
-)
-RKSOK_type = RKSOKProtocolFirstVersion
+from service.request_handler import process_client_request
+from utils import check_host_port
 
 
-async def process_request(reader: StreamReader, writer: StreamWriter) -> None:
+async def process_request(reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
     """Callback for asyncio streams server."""
+    mongo_client = RKSOKMongoClient()
     try:
         client_address = writer.get_extra_info('peername')
         logger.debug(f'New connection from {client_address!r}')
@@ -30,17 +20,12 @@ async def process_request(reader: StreamReader, writer: StreamWriter) -> None:
 
         await mongo_client.connect_to_db(user_id=client_address[0])
         logger.debug('Starting request processing...')
-        response = await process_client_request(
-            request=request,
-            db_client=mongo_client,
-            control_server_conf=control_server_conf,
-            rksok_type=RKSOK_type,
-        )
+        response = await process_client_request(request=request, db_client=mongo_client)
         logger.info(f'Send {response!r} to {client_address!r}')
     except ServerBaseException as server_exception:
         logger.error(f'Exception happened: {server_exception}')
     else:
-        writer.write(response.encode())
+        writer.write(response.encode(encoding=ENCODING))
         await writer.drain()
     finally:
         logger.debug(f'Close the connection to {client_address!r}')
@@ -48,11 +33,20 @@ async def process_request(reader: StreamReader, writer: StreamWriter) -> None:
 
 
 async def main() -> None:
-    server = await asyncio.start_server(client_connected_cb=process_request, host=SERVER_HOST, port=SERVER_PORT)
+    try:
+        check_host_port(host=SERVER_HOST, port=SERVER_PORT)
+    except (IncorrectHostError, IncorrectPortError) as connection_data_error:
+        logger.error(f'Exception during checking host and port of the server: {connection_data_error}')
+        raise KeyboardInterrupt
+    server = await asyncio.start_server(client_connected_cb=process_request, host=SERVER_HOST, port=int(SERVER_PORT))
     logger.debug(f'Started RKSOK server on {SERVER_HOST}:{SERVER_PORT}')
     async with server:
         await server.serve_forever()
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info('Interrupting server...')
+        logger.debug('RKSOK server stopped working.')
